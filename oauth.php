b9c63261a873ec4e3ce435cfe18593616038f34c
chen206 <chen206@gmail.com>
2021-09-01 13:26:21 +0800

    update example

--- oauth.php
+++ oauth.php
@@ -13,9 +13,9 @@ $config = Configuration::forSymmetricSigner(
     InMemory::file(__DIR__ . '/private-key.pem')
 );
 
-$client_id = 'SEARCHADS.27478e71-3bb0-4588-998c-182e2b405577';
-$team_id = 'SEARCHADS.27478e71-3bb0-4588-998c-182e2b405577';
-$key_id = 'bacaebda-e219-41ee-a907-e2c25b24d1b2';
+$client_id = 'SEARCHADS.c99b2f21-5964-4869-847c-ljh799004501';
+$team_id = 'SEARCHADS.c99b2f21-5964-4869-847c-ljh799004501';
+$key_id = '285e95c1-d514-4522-b6e3-ljh799004501';
 
 $audience = "https://appleid.apple.com";
 $alg = "ES256";
@@ -33,13 +33,16 @@ $token = $config->builder()
 
 $client_secret = $token->toString();
 
+echo "clientSecret 建议保存，有效期可设置最长 180 天\n";
 echo $client_secret;
+echo "\n";
 
 // 2. Request an Access Token
 $postdata = http_build_query(
     array(
         'grant_type' => 'client_credentials',
         'scope' => 'searchadsorg',
+        'agency' => 'ljh',
         'client_id' => $client_id,
         'client_secret' => $client_secret
     )
@@ -56,7 +59,9 @@ $context = stream_context_create($opts);
 $contents = file_get_contents('https://appleid.apple.com/auth/oauth2/token', false, $context);
 $results = json_decode($contents); 
 
+echo "access_token 有效期1个小时\n";
 echo $results->access_token;
+echo "\n";
 
 // 3. Get User ACL
 
@@ -70,3 +75,4 @@ $context = stream_context_create($opts);
 $contents = file_get_contents('https://api.searchads.apple.com/api/v4/acls', false, $context);
 
 echo $contents;
+echo "\n";
